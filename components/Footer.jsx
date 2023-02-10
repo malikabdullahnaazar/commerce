8700cf781784d7d93de33774adc24f9600ad73a2
Malik Abdullah Naazar <malikabdullah130037@example.com>
2023-02-10 11:58:28 +0500

    first commit to clone a  project

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link'
+// import  { AiOutlineShoppingCart } from 'react-icon/ai'
+
+import { FaBeer } from 'react-icons/fa';
+const Footer = () => {
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Jsm HeadPhones</Link>
+      </p>
+      <h3> Lets go for a <FaBeer />? </h3>
+      {/* <button type="button" className="cart-icon" onClick=""><AiOutlineShoppingCart /></button> */}
+    </div>
+  )
+}
+
+export default Footer
