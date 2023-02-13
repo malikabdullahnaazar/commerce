1bab49abc84c022d5a0e145acb857da742fd2f25
Malik Abdullah Naazar <malikabdullah130037@example.com>
2023-02-13 12:56:01 +0500

    adding cart component to app and more on stripe payment file

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
-
+import { useStateContext } from "../context/statecontex";
 import { AiOutlineShopping } from 'react-icons/ai'
+import Cart from './Cart';
 const Navbar = () => {
+  const {showCart,setShowCart,totalQuantities}=useStateContext();
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">JSM HeadPhones</Link>
       </p>
-      <button type="button" className="cart-icon" onClick=""><AiOutlineShopping /></button>
-        <span className="cart-item-qty">1</span>
+      <button type="button" className="cart-icon" onClick={()=>setShowCart(true)}><AiOutlineShopping /></button>
+        <span className="cart-item-qty">{totalQuantities}</span>
+      {showCart && <Cart />}
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import {  StateContext } from '../context/statecontex'
 export default function App({ Component, pageProps }: AppProps) {
   return  (
+    <StateContext>
+
     <Layout>
     <Component {...pageProps} />
     </Layout>
+    </StateContext>
   )
 }
